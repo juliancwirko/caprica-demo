459db2257a37e3b49e512f285bb09471416d340c
Julian Ćwirko <julian.cwirko@gmail.com>
2015-02-07 21:34:07 +0100

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+### Meteor with Arduino and Cylon.js - demo app
+
+#### Check out packages:
+
+- [juliancwirko:arduino-cylon](https://github.com/juliancwirko/meteor-arduino-cylon)
+- [juliancwirko:caprica](https://github.com/juliancwirko/meteor-caprica)
+
+#### Demo UI on Meteor.com:
+
+[http://caprica-demo.meteor.com/](http://caprica-demo.meteor.com/)
---
+++ server/cylon.js
@@ -0,0 +1,71 @@
+'use strict';
+
+Meteor.startup(function () {
+
+    Cylon.robot({
+        name: 'Number Six',
+        description: 'Number Six is a humanoid Cylon who plays several key roles in Cylon society. Number Sixes are typically sensual and use seduction in infiltration.',
+
+        // you need to have connection with Arduino board to run this demo app
+        // or you can comment connections and devices and references in functions
+        connections: {
+          arduino: { adaptor: 'firmata', port: '/dev/ttyACM0' }
+        },
+
+        devices: {
+          redLed: { driver: 'led', pin: 13 },
+          yellowLed: { driver: 'led', pin: 12 }
+        },
+
+        redLed: function () {
+            this.devices.redLed.toggle();
+            return 'Cylon ' + this.name + ' toggles red led';
+        },
+
+        yellowLed: function () {
+            this.devices.yellowLed.toggle();
+            return 'Cylon ' + this.name + ' toggles yellow led';
+        },
+
+        toggleAll: function () {
+            this.devices.redLed.toggle();
+            this.devices.yellowLed.toggle();
+            return 'Cylon ' + this.name + ' toggles red and yellow led';
+        },
+
+        commands: function () {
+            return {
+                'Toggle red Led': this.redLed,
+                'Toggle yellow Led': this.yellowLed,
+                'Toggle all': this.toggleAll
+            };
+        }
+    }).start();
+
+    // Cylon.robot({
+    //     name: 'Number Eight',
+
+    //     connections: {
+    //       arduino: { adaptor: 'firmata', port: '/dev/other_arduino_port' }
+    //     },
+
+    //     devices: {
+    //       led: { driver: 'led', pin: 10 }
+    //     },
+
+    //     someCommand: function () {
+    //
+    //     },
+    //     someOtherCommand: function () {
+    //
+    //     },
+    //
+    //     commands: function () {
+    //         return {
+    //             'Some command': this.someCommand,
+    //             'Some other command': this.someOtherCommand
+    //         };
+    //     }
+    // }).start();
+
+});
